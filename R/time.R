3e4db11dda513d00ce8a54ef7103d8361b9f6e6a
flaviomoc <flaviomoc@gmail.com>
2024-12-07 10:17:27 -0300

    Initial commit

---
+++ R/time.R
@@ -0,0 +1,49 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to DF package, which stands for Debora and Flavio! It has a single function: time_since()")
+}
+#' time since initial date
+#'
+#' @return message
+#' @export
+time_since <- function() {
+  # Predefined initial dates
+  civil <- as.Date("2022-10-27")  # Civil date
+  cerimonia <- as.Date("2023-01-05")  # Ceremony date
+  together_since <- as.Date("2014-03-24")  # New date
+
+  # Get the current date
+  current_date <- Sys.Date()
+
+  # Calculate the difference in days for both dates
+  days_passed_civil <- as.numeric(current_date - civil)
+  days_passed_cerimonia <- as.numeric(current_date - cerimonia)
+  days_passed_together <- as.numeric(current_date - together_since)  # Days since together
+
+  # Function to calculate years, months, and days
+  calculate_time <- function(days_passed) {
+    years <- days_passed %/% 365
+    months <- (days_passed %% 365) %/% 30
+    days <- (days_passed %% 365) %% 30
+    return(list(years = years, months = months, days = days))
+  }
+
+  # Calculate time since both dates
+  time_since_civil <- calculate_time(days_passed_civil)
+  time_since_cerimonia <- calculate_time(days_passed_cerimonia)
+  time_since_together <- calculate_time(days_passed_together)  # Time since together
+
+  # Create a message to show the elapsed time
+  elapsed_time <- paste0("Debora and Flavio have been officially married for ",
+                         time_since_civil$years, " years, ",
+                         time_since_civil$months, " months, and ",
+                         time_since_civil$days, " days, but ",
+                         time_since_cerimonia$years, " years, ",
+                         time_since_cerimonia$months, " months, and ",
+                         time_since_cerimonia$days, " days have passed since their ceremony with family. ",
+                         "They are together for ",
+                         time_since_together$years, " years, ",
+                         time_since_together$months, " months, and ",
+                         time_since_together$days, " days.")
+
+  return(elapsed_time)
+}
